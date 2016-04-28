d869f53ebeaa8262945d3fcf6439a51f29789fdd
Laptop <sarathm00@gmail.com>
2016-04-28 23:06:28 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,105 @@
+from PyQt4 import QtGui  # , QtCore
+from py_file import Ui_Dialog
+import sys
+import time
+import serial
+
+PORT = '/dev/ttyUSB0'
+
+
+class VoiceCall:
+    def __init__(self, port):
+        self.port = port
+
+    def dialNumber(self, number='9496354518'):
+        self.number = number
+        self.ser = serial.Serial(self.port)
+        self.ser.write('ATZ\r')
+        # ATZ : Restore profile ##
+        time.sleep(1)
+        self.ser.write('ATD ' + self.number + ';\r')
+        # ATD : Dial command ##
+        # semicolon : voice call ##
+        time.sleep(1)
+        time.sleep(1)
+        self.ser.write(chr(26))
+        time.sleep(1)
+        time.sleep(1)
+        time.sleep(1)
+
+    def endCall(self):
+        self.ser = serial.Serial(self.port)
+        self.ser.write('ATZ\r')
+        time.sleep(1)
+        self.ser.write('AT+CHUP\r')
+        time.sleep(1)
+        self.ser.write(chr(26))
+        time.sleep(1)
+
+
+class TextMessage:
+    def __init__(self, port):
+        self.port = port
+
+    def sendMessage(self, text, number='9496354518'):
+        self.number = number
+        self.text = text
+        self.ser = serial.Serial(self.port)
+        self.ser.write('ATZ\r')
+        time.sleep(1)
+        self.ser.write('AT+CMGF=1\r')
+        time.sleep(1)
+        self.ser.write('''AT+CMGS="''' + self.number + '''"\r''')
+        time.sleep(1)
+        self.ser.write(self.text + "\r")
+        time.sleep(1)
+        self.ser.write(chr(26))
+        time.sleep(1)
+
+
+class Gui(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.ui.call_attendButton.pressed.connect(self.makeCall)
+        self.ui.call_endButton.pressed.connect(self.endCall)
+        self.ui.sms_button.pressed.connect(self.sendSms)
+        self.call = VoiceCall(PORT)
+        self.sms = TextMessage(PORT)
+
+    def sendSms(self):
+        try:
+            number = self.ui.call_number.text()
+            number = str(number)
+            text = self.ui.sms_text.toPlainText()
+            self.sms.sendMessage(text, number)
+
+        except Exception, e:
+            self.ui.call_number.clear()
+            print e
+
+    def makeCall(self):
+        try:
+            number = self.ui.call_number.text()
+            number = str(number)
+
+            self.call.dialNumber(number)
+
+        except Exception, e:
+            self.ui.call_number.clear()
+            print e
+
+    def endCall(self):
+        self.call.endCall()
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = Gui()
+    # app.aboutToQuit.connect(ex.flags.closeSocket)
+    ex.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
---
+++ py_file.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui_file.ui'
+#
+# Created: Thu Apr 28 23:02:57 2016
+#      by: PyQt4 UI code generator 4.10.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName(_fromUtf8("Dialog"))
+        Dialog.resize(400, 300)
+        self.tabWidget = QtGui.QTabWidget(Dialog)
+        self.tabWidget.setGeometry(QtCore.QRect(0, 0, 401, 291))
+        self.tabWidget.setObjectName(_fromUtf8("tabWidget"))
+        self.tab = QtGui.QWidget()
+        self.tab.setObjectName(_fromUtf8("tab"))
+        self.gridLayoutWidget = QtGui.QWidget(self.tab)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(80, 60, 201, 80))
+        self.gridLayoutWidget.setObjectName(_fromUtf8("gridLayoutWidget"))
+        self.gridLayout = QtGui.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setMargin(0)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.call_number = QtGui.QLineEdit(self.gridLayoutWidget)
+        self.call_number.setMaxLength(10)
+        self.call_number.setObjectName(_fromUtf8("call_number"))
+        self.gridLayout.addWidget(self.call_number, 0, 1, 1, 1)
+        self.label = QtGui.QLabel(self.gridLayoutWidget)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.call_attendButton = QtGui.QPushButton(self.tab)
+        self.call_attendButton.setGeometry(QtCore.QRect(90, 160, 85, 27))
+        self.call_attendButton.setObjectName(_fromUtf8("call_attendButton"))
+        self.call_endButton = QtGui.QPushButton(self.tab)
+        self.call_endButton.setGeometry(QtCore.QRect(190, 160, 85, 27))
+        self.call_endButton.setObjectName(_fromUtf8("call_endButton"))
+        self.tabWidget.addTab(self.tab, _fromUtf8(""))
+        self.tab_2 = QtGui.QWidget()
+        self.tab_2.setObjectName(_fromUtf8("tab_2"))
+        self.gridLayoutWidget_2 = QtGui.QWidget(self.tab_2)
+        self.gridLayoutWidget_2.setGeometry(QtCore.QRect(80, 40, 251, 51))
+        self.gridLayoutWidget_2.setObjectName(_fromUtf8("gridLayoutWidget_2"))
+        self.gridLayout_2 = QtGui.QGridLayout(self.gridLayoutWidget_2)
+        self.gridLayout_2.setMargin(0)
+        self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
+        self.sms_number = QtGui.QLineEdit(self.gridLayoutWidget_2)
+        self.sms_number.setMaxLength(10)
+        self.sms_number.setObjectName(_fromUtf8("sms_number"))
+        self.gridLayout_2.addWidget(self.sms_number, 0, 1, 1, 1)
+        self.label_2 = QtGui.QLabel(self.gridLayoutWidget_2)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.gridLayout_2.addWidget(self.label_2, 0, 0, 1, 1)
+        self.sms_button = QtGui.QPushButton(self.tab_2)
+        self.sms_button.setGeometry(QtCore.QRect(170, 200, 71, 27))
+        self.sms_button.setObjectName(_fromUtf8("sms_button"))
+        self.sms_text = QtGui.QTextEdit(self.tab_2)
+        self.sms_text.setGeometry(QtCore.QRect(80, 100, 251, 78))
+        self.sms_text.setObjectName(_fromUtf8("sms_text"))
+        self.tabWidget.addTab(self.tab_2, _fromUtf8(""))
+
+        self.retranslateUi(Dialog)
+        self.tabWidget.setCurrentIndex(1)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(_translate("Dialog", "PiGSM", None))
+        self.label.setText(_translate("Dialog", "Number", None))
+        self.call_attendButton.setText(_translate("Dialog", "Call", None))
+        self.call_endButton.setText(_translate("Dialog", "End", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("Dialog", "Call", None))
+        self.label_2.setText(_translate("Dialog", "Number", None))
+        self.sms_button.setText(_translate("Dialog", "Send", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("Dialog", "SMS", None))
+
